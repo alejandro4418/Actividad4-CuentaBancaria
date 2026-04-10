5a6b510d9ef49d5f83f978160751da6eb89d94ca
alejandro4418 <alejandro.aguirre@epn.edu.ec>
2026-04-09 20:56:55 -0500

    Actividad 4

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
     CuentaBancaria c1 = new CuentaBancaria("Alejandro", 56.89);
-    c1.mostrarInfo();
+    c1.mostrarInformacion();
     c1.depositar(26.8);
     c1.retirar(14.5);
     c1.mostrarInformacion();
